fab4644b3fa40ac240322ef1c3628d6d2b610dff
Pankaj j Patil <pp892810@gmail.com>
2024-04-15 00:49:19 +0530

    sql completed..

--- sql_task/database_store.sql
+++ sql_task/database_store.sql
@@ -14,4 +14,8 @@ select *from groupMembership;
 
 select *from group1 where name like 'TEST-%';
 
+select firstName,lastName from groupMembership gm inner join user on gm.userID=user.id inner join group1 on gm.groupID=group1.id where user.firstName='Victor' and group1.name not like '%TEST%';
+
+select firstName as 'user_firstName',lastName as 'user_lastName',g1.name from groupMembership gm inner join user u on gm.userID=u.id inner join group1 g1 on gm.groupID=g1.id where u.created < g1.created;
+
 
